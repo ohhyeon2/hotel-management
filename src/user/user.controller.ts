c4773a03e4b5b3ceafc3a6aa73b71f30473f99b5
ohhyeon2 <dhgus1235@gmail.com>
2024-05-03 20:25:26 +0900

    feat: auth, user 기본 세팅

---
+++ src/user/user.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Post, Get } from '@nestjs/common';
+
+@Controller('user')
+export class UserController {
+
+  @Post()
+  signup() {
+
+  }
+
+  @Post()
+  login() {
+
+  }
+
+  @Get()
+  info() {
+
+  }
+}
