a6aeac848a477a38624ca0b393d98c96bb9010a2
ohhyeon2 <dhgus1235@gmail.com>
2024-05-13 14:50:46 +0900

    feat: nestJS 내장 exception filter 적용

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   const config = new DocumentBuilder()
     .setTitle('hotel management')
     .setDescription('hotel management API')
     .setVersion('1.0')
     .addTag('management web app')
-    .build()
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api', app, document)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
