41e4e4fde8cab2059054bd407260c62ef8f6b4bd
ohhyeon2 <dhgus1235@gmail.com>
2024-05-13 14:20:48 +0900

    feat: 유저 개인 정보 dto 추가

--- src/user/dto/res.dto.ts
+++ src/user/dto/res.dto.ts
@@ -11,7 +11,15 @@ export class FindUserResDto {
 
   @ApiProperty({ required: true, example: 'nestjs@naver.com' })
   email: string;
+}
+
+export class GetUserResDto {
+  @ApiProperty({ required: true, example: 'nestjs@naver.com' })
+  email: string;
 
   @ApiProperty({ required: true, example: '010-1234-5678' })
   phone: string;
+
+  @ApiProperty({ required: true, example: '닉네임' })
+  nickname: string;
 }
