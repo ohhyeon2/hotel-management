d2d49619238cd68aa00812f001e36d9f95a7421b
ohhyeon2 <dhgus1235@gmail.com>
2024-05-13 17:28:16 +0900

    feat: refresh token 인증 추가

--- src/auth/jwt/jwt-auth.guard.ts
+++ src/auth/jwt/jwt-auth.guard.ts
@@ -1,11 +1,12 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
 import { IS_PUBLIC_KEY } from 'src/common/decorator/public.decorator';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(private reflector: Reflector) {
+  constructor(private reflector: Reflector, private jwtService: JwtService) {
     super();
   }
 
@@ -17,6 +18,16 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     if (isPublic) {
       return true;
     }
+
+    const http = context.switchToHttp();
+    const { url, headers } = http.getRequest<Request>(); 
+    const token = /Bearer\s(.+)/.exec(headers['authorization'])[1]
+    const decodeToken = this.jwtService.decode(token)
+
+    if (url !== '/auth/refresh' && decodeToken['tokenType'] === 'refresh') {
+      throw new UnauthorizedException();
+    }
+
     return super.canActivate(context);
   }
 }
