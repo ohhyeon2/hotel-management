6e7c994a63b76b301ff5d22f5d254808a1779f60
ohhyeon2 <dhgus1235@gmail.com>
2024-05-14 10:05:43 +0900

    feat: 리프레쉬 토큰 재발급 로직 작성

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,11 @@
-import { BadRequestException, Body, Controller, Post, Headers } from '@nestjs/common';
+import { Body, Controller, Post, Headers } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SigninReqDto } from './dto/req.dto';
 import { RefreshTokenResDto, SigninResDto } from './dto/res.dto';
-import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
-import { User } from 'src/user/entity/user.entity';
+import { ApiBearerAuth, ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { Public } from 'src/common/decorator/public.decorator';
+import { UserAfterAuth } from 'src/common/decorator/user.decorator';
+import { User } from 'src/common/decorator/user.decorator';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -11,15 +13,17 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @ApiCreatedResponse({type: SigninResDto})
+  @Public()
   @Post('signin')
   async signin(@Body() {email, password}: SigninReqDto): Promise<SigninResDto> {
     return this.authService.signin(email, password)
   }
 
   @ApiCreatedResponse({type: RefreshTokenResDto})
+  @ApiBearerAuth()
   @Post('refresh')
-  async refresh(@Headers('authorization') authorization, user: User): Promise<RefreshTokenResDto> {
-    const token = /Bearer\/s(.+)/.exec(authorization)[1];
+  async refresh(@Headers('authorization') authorization, @User() user: UserAfterAuth): Promise<RefreshTokenResDto> {
+    const token = /Bearer\s(.+)/.exec(authorization)[1];
     const { accessToken, refreshToken } = await this.authService.refresh(token, user.id)
     return { accessToken, refreshToken }
   }
