d795d2c1dd662c485f34e2f1073385ae2eae1653
ohhyeon2 <dhgus1235@gmail.com>
2024-05-14 23:53:46 +0900

    refactor: email.service의 랜덤 문자열 생성 메서드를 email.controller로 이동

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -3,15 +3,24 @@ import { EmailService } from './email.service';
 import { ApiTags } from '@nestjs/swagger';
 import { EmailReqDto } from './dto/req.dto';
 import { Public } from 'src/common/decorator/public.decorator';
+import * as crypto from 'crypto';
 
 @ApiTags('Email')
 @Controller('email')
 export class EmailController {
-  constructor(private readonly emailService: EmailService) {}
+  constructor(
+    private readonly emailService: EmailService,
+  ) {}
 
   @Public()
   @Post()
   async send(@Body() { email }: EmailReqDto) {
-    this.emailService.send(email);
+    const code = this.generateRandomString(6);
+    this.emailService.send(email, code);
+  }
+
+  private generateRandomString(length: number): string {
+    const randomBytes = crypto.randomBytes(length).toString('base64').slice(0, length)
+    return randomBytes;
   }
 }
--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,13 +1,11 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
-import * as crypto from 'crypto';
 
 @Injectable()
 export class EmailService {
   constructor(private readonly mailerService: MailerService) {}
 
-  async send(email: string) {
-    const code = this.generateRandomString(6);
+  async send(email: string, code: string) {
     await this.mailerService.sendMail({
       to: email,
       from: 'dhgus1235@gmail.com',
@@ -15,10 +13,5 @@ export class EmailService {
       text: '회원가입 코드입니다.',
       html: `<br>회원가입 인증 코드입니다.<br>${code}</br>`,
     });
-  }
-
-  private generateRandomString(length: number): string {
-    const randomBytes = crypto.randomBytes(length).toString('base64').slice(0, length)
-    return randomBytes;
   }
 }
