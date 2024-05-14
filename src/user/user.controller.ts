2e5fc85a90aaebafbd7775a5fa13eb6c9c52ef30
ohhyeon2 <dhgus1235@gmail.com>
2024-05-14 10:06:15 +0900

    feat: 유저 개인정보 확인 작성

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Post, Get, Param, Body } from '@nestjs/common';
-import { ApiCreatedResponse, ApiExtraModels, ApiTags } from '@nestjs/swagger';
+import { Controller, Post, Get, Param, Body, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiCreatedResponse, ApiExtraModels, ApiTags } from '@nestjs/swagger';
 import { UserService } from 'src/user/user.service';
 import { FindUserReqDto, SignupReqDto } from './dto/req.dto';
 import { GetUserResDto, SignupResDto } from './dto/res.dto';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt-auth.guard';
+import { Public } from 'src/common/decorator/public.decorator';
 
 @ApiTags('User')
 @ApiExtraModels(FindUserReqDto)
@@ -11,12 +13,15 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @ApiCreatedResponse({type: SignupResDto})
+  @Public()
   @Post('signup')
   async create(@Body() signupReqDto: SignupReqDto): Promise<SignupResDto> {
     const { id } = await this.userService.createUser(signupReqDto);
     return { id };
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async getUser(@Param() { id }: FindUserReqDto): Promise<GetUserResDto> {
     return this.userService.findOneByUserId(id);
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,12 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   exports: [UserService],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService, JwtService]
 })
 export class UserModule {}
