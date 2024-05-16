b24ff97b4949b9f7d5c47af15f63d8e192b18749
ohhyeon2 <dhgus1235@gmail.com>
2024-05-16 18:10:46 +0900

    chore: 에러 로그 추가
    에러 발생시 status code, error message 로깅

--- src/common/exceptions/http-exception.filter.ts
+++ src/common/exceptions/http-exception.filter.ts
@@ -1,18 +1,26 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from "@nestjs/common";
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, Logger } from "@nestjs/common";
 import { Response, Request } from "express";
+import { LoggerMiddleware } from "../middleware/logger.middleware";
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+
+  private readonly logger = new Logger(LoggerMiddleware.name)
+
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
+    const message = exception.getResponse();
+
+    this.logger.error(`HTTP Status: ${status}] : Error Message: ${message}`)
 
     response.status(status).json({
       statusCode: status,
       timeStamp: new Date().toISOString(),
-      pash: request.url,
+      path: request.url,
+      message: message,
     })
   }
 }
